8d0d6c60da9ef7ac15be61205879c1eb964810ce
André Ferreira <andreferreiragemeo@gmail.com>
2024-12-15 11:06:48 +0000

    First commit

--- content-calendar/src/main/java/andre/dev/content_calendar/config/DataLoader.java
+++ content-calendar/src/main/java/andre/dev/content_calendar/config/DataLoader.java
@@ -2,15 +2,14 @@ package andre.dev.content_calendar.config;
 
 import andre.dev.content_calendar.model.Content;
 import andre.dev.content_calendar.repository.ContentRepository;
-import com.fasterxml.jackson.core.type.TypeReference;
-import com.fasterxml.jackson.databind.ObjectMapper;
-
-import org.springframework.boot.CommandLineRunner;
-import org.springframework.context.annotation.Profile;
-import org.springframework.stereotype.Component;
-
 import java.io.InputStream;
 import java.util.List;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 @Component
 public class DataLoader implements CommandLineRunner {
